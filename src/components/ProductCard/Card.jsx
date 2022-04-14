b880db4d7286bf3a55d590af878d748f3af64959
jose_miguel <josecastrillong@gmail.com>
2022-04-13 19:56:36 -0500

    feat: timer on product card

--- src/components/ProductCard/Card.jsx
+++ src/components/ProductCard/Card.jsx
@@ -1,9 +1,24 @@
 import { useQuery } from 'react-query'
+import { useState, useEffect } from 'react';
 import { getProducts } from '../../api/store_items'
 import styles from './Card.module.css'
 
 
 function Card() {
+
+  const [time, setTime] = useState(Math.round(Math.random() * (3600 - 10)));
+  const clockTime = {
+    minutes: Math.floor((time /  60) % 60),
+    seconds: Math.floor((time % 60 ))
+  };
+
+  useEffect(() => {
+    if (time > 0) {
+      setTimeout(() => setTime(time - 1), 1000); 
+    } else {
+      setTime('Offer is over')
+    }
+  })
 
   const { isLoading, isError, data, error } = useQuery('todos', getProducts)
 
@@ -21,6 +36,7 @@ function Card() {
     <article className={styles.card_container}>
       <section className={styles.image_section}>
         <img className={styles.image} src={data[1].image} alt={data[1].title}/>
+        <div className={styles.tick_tac}>{ time > 0 ? `${clockTime.minutes} min ${clockTime.seconds} seconds` : time}</div>
       </section>
       <section className={styles.title_section}>
         <span className={styles.product_title}>{data[1].title}</span>
